6f1d872430ad9d58192b47d6c34b7e656089e142
Kyle Halstvedt <khalstvedt@gmail.com>
2014-06-13 05:45:29 -0600

    moved some stuff

--- start.sh
+++ start.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
-scp setup_node_ubuntu.sh root@$1:"~/"
-ssh root@$1 "chmod a+x ~/setup_node_ubuntu.sh; screen -Sdm setup ~/setup_node_ubuntu.sh"
+# copy setup script
+scp ./setup_ubuntu.sh root@$1:"~/"
+
+# run script under new 'setup' screen
+ssh root@$1 "screen -Sdm setup /bin/bash ~/setup_ubuntu.sh"
