619355dee6967f722094e8fda1660cf6cf8578db
Kyle Halstvedt <khalstvedt@gmail.com>
2014-06-13 07:00:30 -0600

    fixed output formatting

--- start.sh
+++ start.sh
@@ -4,19 +4,19 @@
 if [ -n "$1" ]; then
     if [ "$1" = "-r" ]; then
         if  [ -n "$2" ]; then
-            printf "Executing remote install at $2"
+            printf "Executing remote install at $2 ...\n"
             # copy setup script
             scp ./setup_ubuntu.sh root@$2:"~/"
 
             # run script under new 'setup' screen
             ssh root@$2 "screen -Sdm setup /bin/bash ~/setup_ubuntu.sh"
         else
-            printf "Remote flag requires an address parameter."
+            printf "Remote flag requires an address parameter.\n"
         fi
     else
-        printf "Invalid option. You may use the -r option with an address parameter only."
+        printf "Invalid option. You may use the -r option with an address parameter only.\n"
     fi
 else
-    printf "Executing local install"
+    printf "Executing local install...\n"
     screen -Sdm setup /bin/bash ./setup_ubuntu.sh
 fi
