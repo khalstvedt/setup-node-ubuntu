105e2a2553ffdc258dc66684bd377b8504081c67
Kyle Halstvedt <khalstvedt@gmail.com>
2014-06-13 00:01:39 -0600

    Refactoring

--- setup_node_ubuntu.sh
+++ setup_node_ubuntu.sh
@@ -1,9 +1,7 @@
-sudo apt-get update
-sudo apt-get upgrade
-sudo apt-get -y install emacs git npm
-curl https://raw.githubusercontent.com/creationix/nvm/v0.7.0/install.sh | sh
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
 echo 'deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen' | sudo tee /etc/apt/sources.list.d/mongodb.list
-sudo apt-get update
-sudo apt-get -y install mongodb-org
+sudo apt-get -y update
+sudo apt-get -y upgrade
+sudo apt-get -y install emacs npm mongodb-org
+curl https://raw.githubusercontent.com/creationix/nvm/v0.7.0/install.sh | sh
 curl https://install.meteor.com | /bin/sh
