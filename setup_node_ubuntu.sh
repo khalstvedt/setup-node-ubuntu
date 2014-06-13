c7e957573ec48d4c181d098ddbb4df6c5a7c292a
Kyle Halstvedt <khalstvedt@gmail.com>
2014-06-13 04:52:14 -0400

    reorganized

--- setup_node_ubuntu.sh
+++ setup_node_ubuntu.sh
@@ -1,18 +1,40 @@
 #!/bin/bash
-#
-#
+
 # get mongo key
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
 # write mongo list file
 echo 'deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen' | sudo tee /etc/apt/sources.list.d/mongodb.list
+
 # update apt cache
 sudo apt-get -y update
 # install updates
 sudo apt-get -y upgrade
-# install basics
-sudo apt-get -y install emacs npm mongodb-org
-# install nvm
+
+# install emacs
+sudo apt-get -y install emacs
+
+# install mongodb
+sudo apt-get -y install mongodb-org
+
+# install nvm, node
+sudo apt-get -y install npm
 curl https://raw.githubusercontent.com/creationix/nvm/v0.7.0/install.sh | sh
+source ~/.nvm/nvm.sh
 printf "source ~/.nvm/nvm.sh\n" >> ~/.bashrc
+nvm install 0.11
+
+# install pip, venv (assumes python 2.7 pre-installed)
+sudo apt-get -y install python-pip
+sudo pip install virtualenv virtualenvwrapper
+
+# install rvm, ruby, rails
+curl -L https://get.rvm.io | bash -s stable --autolibs=enabled --ruby --rails
+source /usr/local/rvm/scripts/rvm
+printf "source /usr/local/rvm/scripts/rvm\n" >> ~/.bashrc
+
 # install meteor
 curl https://install.meteor.com | /bin/sh
+
+# configure git
+git config --global user.email "khalstvedt@gmail.com"
+git config --global user.name "Kyle Halstvedt"
